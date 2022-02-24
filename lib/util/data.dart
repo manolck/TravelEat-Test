94476975e220aa374c7e0e6f74414c8e03826057
manolck <chrisophe.noroy@live.fr>
2022-02-24 10:30:17 -0400

    Initial commit

---
+++ lib/util/data.dart
@@ -0,0 +1,34 @@
+List furnitures = [
+  {
+    "name": "Dombrés de ouassous",
+    "img": "assets/1.jpg",
+    "desc":
+        "Qui aurait dit que de simples boulettes faîtes principalement d’eau et de farine avaient si bon goût agrémentées de ouassous ? Un régal !",
+    "prix": "\€15.00",
+    "id": 10,
+  },
+  {
+    "name": "Bokit et Agoulou",
+    "img": "assets/4.jpg",
+    "desc":
+        "Appétit d’oiseau s’abstenir ! “Plat” typique, classique, presque cliché…Cependant lorsque l’on en a été privé pendant une longue période, déguster un bon bokit ou un agoulou reste un incontournable !",
+    "prix": "\€7.00",
+    "id": 11,
+  },
+  {
+    "name": "Court-bouillon de poisson",
+    "img": "assets/3.jpg",
+    "desc":
+        "Du poisson fraichement pêché, macéré dans son assaisonnement qui aura patiemment mijoté…votre odorat ne résistera pas et votre estomac non plus !",
+    "prix": "\€12.00",
+    "id": 12,
+  },
+  {
+    "name": "Colombo de cabri",
+    "img": "assets/2.jpg",
+    "desc":
+        "Voilà un autre incontournable de votre liste de plats typiques de Guadeloupe à déguster pendant les vacances. Qui peut se détourner du fumet d’un Colombo de cabri ?",
+    "prix": "\€14.00",
+    "id": 13,
+  },
+];
